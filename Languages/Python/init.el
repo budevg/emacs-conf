cd47cd91953385f9016b0a29427bbf26c576e6b7
strelok7 <strelok7@piter.localdomain>
2009-06-29 22:38:29 +0300

    remove ipython support for now

--- Languages/Python/init.el
+++ Languages/Python/init.el
@@ -1,12 +1,6 @@
 
 (autoload 'python-mode "python-mode" "python-mode autoload" t)
 
-(eval-after-load "python-mode"
-  '(progn 
-     (if (require 'ipython "ipython" t)
-         t
-       (warn "package <ipython> is missing use apt-get ipython to install"))))
-
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'auto-mode-alist '("\\.scons$" . python-mode))
 (add-to-list 'auto-mode-alist '("SConstruct\\'" . python-mode))
