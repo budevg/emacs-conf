d5822371e14903d7f6c98c8776234f4128db99d5
strelok7 <strelok7@vostok>
2009-09-09 22:53:59 +0300

    rename documentation to publishing

--- Languages/Ruby/init.el
+++ Languages/Ruby/init.el
@@ -1,17 +1,8 @@
 
 (eval-after-load "ruby-mode"
   '(progn 
-      (require 'inf-ruby)
-      (require 'ruby-electric)
-      
-      
-      (add-hook 'ruby-mode-hook
-                '(lambda ()
-                   (inf-ruby-keys)
-                   (ruby-electric-mode)
-                   ))
-
-      (define-key ruby-mode-map "\C-m" 'newline-and-indent)))
+      (define-key ruby-mode-map "\C-m" 'newline-and-indent)
+      ))
 
 
 
--- Shell/man-pages.el
+++ Shell/man-pages.el
@@ -1,5 +1,5 @@
 
-(require 'iman)
+(autoload 'iman "iman" "iman autoload" t)
 (global-set-key [f1] 'man)
 (global-set-key [(control f1)] 'iman)
 (global-set-key [(shift f1)] 'info)
