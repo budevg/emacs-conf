a3f91638b769afb56af64fa575edfaa866df4edf
strelok7 <strelok7@vostok>
2009-09-28 17:41:57 +0200

    improve ido-goto-symbol

--- Navigation/symbols-navigate.el
+++ Navigation/symbols-navigate.el
@@ -3,7 +3,7 @@
 (setq imenu-auto-rescan t)
 
 (defun ido-goto-symbol ()
-  "Update the imenu index and then use ido to select a symbol to navigate to."
+  "Will update the imenu index and then use ido to select a symbol to navigate to"
   (interactive)
   (imenu--make-index-alist)
   (let ((name-and-pos '())
@@ -15,22 +15,26 @@
                              (cond
                               ((and (listp symbol) (imenu--subalist-p symbol))
                                (addsymbols symbol))
-                              
                               ((listp symbol)
                                (setq name (car symbol))
                                (setq position (cdr symbol)))
-                              
                               ((stringp symbol)
                                (setq name symbol)
                                (setq position (get-text-property 1 'org-imenu-marker symbol))))
-                             
                              (unless (or (null position) (null name))
                                (add-to-list 'symbol-names name)
                                (add-to-list 'name-and-pos (cons name position))))))))
-      (addsymbols imenu--index-alist))
-    (let* ((selected-symbol (ido-completing-read "Symbol? " symbol-names))
-           (position (cdr (assoc selected-symbol name-and-pos))))
-      (goto-char position))))
+      (addsymbols imenu--index-alist)
+      (let* ((symbol-at-point (symbol-name (symbol-at-point)))
+             (selected-symbol (ido-completing-read
+                               "Symbol? "
+                               (if (member symbol-at-point symbol-names)
+                                   (cons symbol-at-point (remove-if (lambda (x) (string-equal x symbol-at-point))
+                                                                    symbol-names))
+                                 symbol-names)))
+             (position (cdr (assoc selected-symbol name-and-pos))))
+        (if (markerp position)
+            (goto-char position) (goto-char (overlay-start position)))))))
 
 (global-set-key [(control \')] 'ido-goto-symbol)
 
