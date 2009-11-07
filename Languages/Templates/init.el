6c6152b9a3bc7a5fdf2d0da76bbcabd04111ee37
strelok7 <strelok7@proshivka>
2009-11-07 16:52:35 +0200

    update yasnippet

--- Languages/Templates/init.el
+++ Languages/Templates/init.el
@@ -6,6 +6,7 @@
   (setq yas/prompt-functions '(yas/ido-prompt
                                yas/dropdown-prompt
                                yas/completing-prompt))
+  (setq yas/use-menu 'abbreviate)
   (yas/load-directory "~/.emacs.d/Languages/Templates/snippets"))
 
 (run-with-idle-timer 5 nil 'load-yasnippet)
