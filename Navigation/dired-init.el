cf397a4fc8c275f28602e683ee7132a383ef2481
strelok7 <strelok7@proshivka>
2009-12-17 21:04:10 +0200

    view  large files with less and sort in dired mode

---
+++ Navigation/dired-init.el
@@ -0,0 +1,57 @@
+
+(eval-after-load "dired"
+  '(progn
+     (defun my-dired-init ()
+       ;; make dired reuse the current buffer instead of opening new buffer
+       ;; for each directory
+       (autoload 'dired-single-buffer "dired-single" "" t)
+       (autoload 'dired-single-buffer-mouse "dired-single" "" t)
+       (autoload 'dired-single-magic-buffer "dired-single" "" t)
+       (autoload 'dired-single-toggle-buffer-name "dired-single" "" t)
+       (define-key dired-mode-map [return] 'dired-single-buffer)
+       (define-key dired-mode-map [mouse-1] 'dired-single-buffer-mouse)
+       (define-key dired-mode-map "^"
+         (function
+           (lambda nil (interactive) (dired-single-buffer "..")))))
+
+     (defun dired-visit-file-with-less ()
+       (interactive)
+       (let ((file-path (dired-get-file-for-visit)))
+         (multi-term)
+         (let ((term-proc (get-buffer-process (current-buffer))))
+           (term-simple-send term-proc
+                             (format "less -j4 %s" file-path)))))
+    
+     (define-key dired-mode-map [(meta return)] 'dired-visit-file-with-less)
+
+     (defun dired-sort ()
+       (interactive)
+       (let ((sort-flags-table (make-hash-table))
+             (sort-types-list '()))
+         (dolist (entry '(("normal". "")
+                          ("rtime". "t")
+                          ("time" . "rt")
+                          ("rsize" . "S")
+                          ("size" . "rS")
+                          ("rname" . "X")
+                          ("name" . "rX")
+                          ("rdir" . "U")
+                          ("dir" . "rU")))
+           (puthash (car entry) (cdr entry) sort-flags-table)
+           (setq sort-types-list (cons (car entry) sort-types-list)))
+         
+         (let ((sort-flag (ido-completing-read
+                           "Dired Sort: "
+                           sort-types-list)))
+           (setq dired-actual-switches (concat "-al" (gethash sort-flag sort-flags-table "")))
+           (revert-buffer))))
+     (define-key dired-mode-map "s" 'dired-sort)
+    
+     (if (boundp 'dired-mode-map)
+         (my-dired-init)
+       (add-hook 'dired-load-hook 'my-dired-init))
+    
+     (define-key dired-mode-map [(control x) (control q)] 'wdired-change-to-wdired-mode)
+     (require 'dired-x)
+     (setq dired-omit-extensions (append dired-omit-extensions
+                                         '(".ko" ".ko.cmd" ".mod.c" ".mod.o.cmd" ".o.cmd") ))))
