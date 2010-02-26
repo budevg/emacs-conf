4c0166227b263efb3f29b5fa2b131446b5e0c9cc
strelok7 <strelok7@proshivka>
2010-02-26 15:20:00 +0200

    add dtrt-indent mode

--- Languages/source-code.el
+++ Languages/source-code.el
@@ -1,5 +1,7 @@
 
 (setq-default indent-tabs-mode nil)
+(autoload 'dtrt-indent-adapt "dtrt-indent" "Adapt to foreign indentation offsets" t)
+(global-set-key [(control x) t] 'dtrt-indent-adapt)
 
 (setq show-paren-style 'expression)
 (setq show-paren-delay 0)
