8f92208695579bc790231d5dfa629317c95eb6b7
strelok7 <strelok7@proshivka>
2010-04-02 01:23:38 +0300

    kernel coding style

--- Languages/CC/init.el
+++ Languages/CC/init.el
@@ -1,15 +1,36 @@
-
+(setq kernel-coding-style nil)
+(defun kernel-coding-style()
+  (interactive)
+  (if kernel-coding-style
+      (progn
+        (setq kernel-coding-style nil)
+        (message "kernel style disabled"))
+    (progn
+      (setq kernel-coding-style t)
+      (message "kernel style enabled"))))
+      
 (eval-after-load "cc-mode"
   '(progn 
-     (defun my-cc-mode-hook ()
+     (defun my-c-mode-hook ()
+       (c-set-style "linux")
+       (if (not kernel-coding-style)
+           (progn
+             (setq c-basic-offset 2)
+             (setq tab-width 2)))
+       (if kernel-coding-style
+           (setq indent-tabs-mode t))
+       (font-lock-add-keywords nil
+                 '(("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face t))))
+     
+     (defun my-c++-mode-hook ()
        (c-set-style "linux")
        (setq c-basic-offset 2)
        (setq tab-width 2)
        (font-lock-add-keywords nil
-                 '(("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face t))))
+                               '(("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face t))))
 
-     (add-hook 'c-mode-hook 'my-cc-mode-hook)
-     (add-hook 'c++-mode-hook 'my-cc-mode-hook)
+     (add-hook 'c-mode-hook 'my-c-mode-hook)
+     (add-hook 'c++-mode-hook 'my-c++-mode-hook)
 
 
      (define-key c-mode-map "\C-m" 'newline-and-indent)
