0a0b728dd6ef2bc314773fd56f4673a5e7becfbf
budevg <budevg@gmail.com>
2011-02-01 21:36:43 +0200

    add slime mode for lisp programming

--- Languages/Lisp/init.el
+++ Languages/Lisp/init.el
@@ -1,4 +1,10 @@
 
 (define-key lisp-mode-shared-map  "\C-m" 'newline-and-indent)
 
-(setq inferior-lisp-program "sbcl")
+(setq inferior-lisp-program "clisp")
+
+(autoload 'slime "slime" nil t) 
+(autoload 'slime-mode "slime" nil t)
+(autoload 'slime-lisp-mode-hook "slime")
+
+(add-hook 'lisp-mode-hook 'slime-lisp-mode-hook)
