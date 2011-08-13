6a058028af29a304dab8d24774c143dbe26628f7
budevg <budevg@gmail.com>
2011-08-14 00:16:09 +0300

    update clojure mode version

--- Languages/Clojure/init.el
+++ Languages/Clojure/init.el
@@ -1,3 +1,5 @@
-(autoload 'clojure-mode "clojure-mode" "clojure-mode autoload" t)
 
-(add-to-list 'auto-mode-alist '("\\.clj\\'" . clojure-mode))
+(autoload 'clojure-mode "clojure-mode" "clojure-mode autoload" t nil)
+(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+(add-hook 'clojure-mode-hook
+          (lambda () (set (make-local-variable 'inferior-lisp-program) "~/tools/bin/clojure")))
