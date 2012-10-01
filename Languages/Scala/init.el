f28d062cc966c529ec673aebd7bd2c0f773c15ab
Evgeny Budilovsky <budevg@gmail.com>
2012-10-02 00:48:15 +0200

    when pressing f1 in scala mode don't trigger the quick toolbar

--- Languages/Scala/init.el
+++ Languages/Scala/init.el
@@ -6,4 +6,5 @@
      (define-key scala-mode-map "\C-m" 'newline-and-indent)
      (define-key scala-mode-map [(control tab)] nil)
      (define-key scala-mode-map [(control c)(control c)] 'scala-eval-buffer)
+     (define-key scala-mode-map [f1] nil)
      ))
