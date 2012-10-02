0774c42571aba9e4f9725c09a245213224392f83
Evgeny Budilovsky <budevg@gmail.com>
2012-10-02 23:30:52 +0200

    imenu for scala

--- Languages/Scala/init.el
+++ Languages/Scala/init.el
@@ -7,4 +7,19 @@
      (define-key scala-mode-map [(control tab)] nil)
      (define-key scala-mode-map [(control c)(control c)] 'scala-eval-buffer)
      (define-key scala-mode-map [f1] nil)
+     (add-hook 'scala-mode-hook
+               (lambda ()
+                 ;;(scala-electric-mode)
+                 (setq imenu-generic-expression
+                       '(
+                         ("var" "\\(var +\\)\\([^(): ]+\\)" 2)
+                         ("val" "\\(val +\\)\\([^(): ]+\\)" 2)
+                         ("override def" "^[ \\t]*\\(override\\) +\\(def +\\)\\([^(): ]+\\)" 3)
+                         ("implicit def" "^[ \\t]*\\(implicit\\) +\\(def +\\)\\([^(): ]+\\)" 3)
+                         ("def" "^[ \\t]*\\(def +\\)\\([^(): ]+\\)" 2)
+                         ("trait" "\\(trait +\\)\\([^(): ]+\\)" 2)
+                         ("class" "^[ \\t]*\\(class +\\)\\([^(): ]+\\)" 2)
+                         ("case class" "^[ \\t]*\\(case class +\\)\\([^(): ]+\\)" 2)
+                         ("object" "\\(object +\\)\\([^(): ]+\\)" 2)
+                         ))))
      ))
