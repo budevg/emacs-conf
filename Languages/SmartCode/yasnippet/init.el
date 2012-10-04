8f7f7e34d696c6e3a255fbb2f5fbb3ffd86c7810
Evgeny Budilovsky <budevg@gmail.com>
2012-10-04 23:06:21 +0200

    update to yasnippet 0.8

--- Languages/SmartCode/yasnippet/init.el
+++ Languages/SmartCode/yasnippet/init.el
@@ -1,30 +1,9 @@
-(defun yas/advise-indent-function (function-symbol)
-    (eval `(defadvice ,function-symbol (around yas/try-expand-first activate)
-             ,(format
-               "Try to expand a snippet before point, then call `%s' as usual"
-               function-symbol)
-             (let ((yas/fallback-behavior nil))
-               (unless (and (interactive-p)
-                            (yas/expand))
-                 ad-do-it)))))
 
-(defun yasnippet-org-mode-fix ()
-  (add-hook 'org-mode-hook
-          (let ((original-command (lookup-key org-mode-map [tab])))
-            `(lambda ()
-               (setq yas/fallback-behavior
-                     '(apply ,original-command))
-               (local-set-key [tab] 'yas/expand)))))
 (defun load-yasnippet ()
+  (setq yas-snippet-dirs "~/.emacs.d/Languages/SmartCode/yasnippet/snippets")
+  (setq yas-verbosity 0)
   (require 'yasnippet)
-  (yas/initialize)
-  (setq yas/prompt-functions '(yas/ido-prompt
-                               yas/dropdown-prompt
-                               yas/completing-prompt))
-  (setq yas/use-menu 'abbreviate)
-  (yas/load-directory "~/.emacs.d/Languages/SmartCode/yasnippet/snippets")
-  (eval-after-load 'org
-    '(progn
-       (yasnippet-org-mode-fix))))
+  (yas-global-mode 1)
+  )
 
 (run-with-idle-timer 5 nil 'load-yasnippet)
