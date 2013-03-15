c52b69122ce30f5915d330758f4f5c18692564b5
Evgeny Budilovsky <budevg@gmail.com>
2013-03-15 16:54:46 +0200

    use bury/unbury-buffer to switch buffers

--- Navigation/buffer-manipulate.el
+++ Navigation/buffer-manipulate.el
@@ -120,5 +120,5 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer-other-window)
 
 ;; rotate buffers
-(global-set-key [(meta right)] 'previous-buffer)
-(global-set-key [(meta left)]  'next-buffer)
+(global-set-key [(meta right)] 'unbury-buffer)
+(global-set-key [(meta left)]  'bury-buffer)
