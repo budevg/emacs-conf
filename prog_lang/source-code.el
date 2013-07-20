ee01679bd8e18d19f1473afcca730c21769b426e
Evgeny Budilovsky <budevg@gmail.com>
2013-07-20 10:01:20 +0300

    open line before/after

--- prog_lang/source-code.el
+++ prog_lang/source-code.el
@@ -19,7 +19,25 @@
             (indent-tabs-mode . nil))))
 "))
 
+(defun smart-open-line ()
+  "Insert an empty line after the current line.
+Position the cursor at its beginning, according to the current mode."
+  (interactive)
+  (move-end-of-line nil)
+  (newline-and-indent))
 
+(defun smart-open-line-above ()
+  "Insert an empty line above the current line.
+Position the cursor at it's beginning, according to the current mode."
+  (interactive)
+  (move-beginning-of-line nil)
+  (newline-and-indent)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(add-hook 'prog-mode-hook '(lambda ()
+                             (local-set-key (kbd "M-o") 'smart-open-line)
+                             (local-set-key (kbd "M-O") 'smart-open-line-above)))
 ;;(require 'judge-indent)
 ;;(global-judge-indent-mode t)
 ;;(setq judge-indent-major-modes '(c-mode python-mode sh-mode))
