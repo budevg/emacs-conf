45b803b194c376ee3985b0e2f24f43bbff52aa86
Evgeny Budilovsky <budevg@gmail.com>
2014-01-06 22:29:50 +0200

    go to the end of line when matching comint history

--- shell/shell-buffer.el
+++ shell/shell-buffer.el
@@ -31,6 +31,28 @@
   '(progn
      ;;["black" "red" "green" "yellow" "blue" "magenta" "cyan" "white"]
      ;; change blue->yellow
+     (defun comint-previous-matching-input-from-input (n)
+       "Search backwards through input history for match for current input.
+\(Previous history elements are earlier commands.)
+With prefix argument N, search for Nth previous match.
+If N is negative, search forwards for the -Nth following match."
+       (interactive "p")
+       (let ((opoint (point)))
+         (unless (memq last-command '(comint-previous-matching-input-from-input
+                                      comint-next-matching-input-from-input))
+           ;; Starting a new search
+           (setq comint-matching-input-from-input-string
+                 (buffer-substring
+                  (or (marker-position comint-accum-marker)
+                      (process-mark (get-buffer-process (current-buffer))))
+                  (point))
+                 comint-input-ring-index nil))
+         (comint-previous-matching-input
+          (concat "^" (regexp-quote comint-matching-input-from-input-string))
+          n)
+         ;;(goto-char opoint)
+         ))
+
      (setq ansi-color-names-vector ["black" "red" "green" "yellow" "yellow" "magenta" "cyan" "white"])
      (ansi-color-for-comint-mode-on)
      (define-key comint-mode-map [C-up] 'comint-previous-matching-input-from-input)
