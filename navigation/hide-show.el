43179d4a964ebc4b0dab0c641d7c5cad7064d140
Evgeny Budilovsky <evgeny.budilovsky@ravellosystems.com>
2014-07-07 10:17:06 +0300

    hide-show xml mode

--- navigation/hide-show.el
+++ navigation/hide-show.el
@@ -20,4 +20,5 @@
      (add-hook 'cperl-mode-hook 'hide-show-hook)
      (add-hook 'java-mode-hook 'hide-show-hook)
      (add-hook 'js-mode-hook 'hide-show-hook)
+     (add-hook 'nxml-mode-hook 'hide-show-hook)
      (add-hook 'sh-mode-hook 'hide-show-hook)))
--- prog_lang/html/init.el
+++ prog_lang/html/init.el
@@ -2,7 +2,7 @@
   "Replace  “&lt;” to “<” and other chars in HTML.
 This works on the current region."
   (interactive "r")
-  (save-restriction 
+  (save-restriction
     (narrow-to-region start end)
     (goto-char (point-min))
     (while (search-forward "&amp;" nil t) (replace-match "&" nil t))
@@ -11,3 +11,28 @@ This works on the current region."
     (goto-char (point-min))
     (while (search-forward "&gt;" nil t) (replace-match ">" nil t))
     ))
+
+
+(eval-after-load "nxml-mode"
+  '(progn
+     (defun nxml-forward-element()
+       (let ((nxml-sexp-element-flag))
+         (setq nxml-sexp-element-flag (not (looking-at "<!--")))
+         (unless (looking-at outline-regexp)
+           (condition-case nil
+               (nxml-forward-balanced-item 1)
+             (error nil)))))
+
+     (add-to-list 'hs-special-modes-alist
+                  '(nxml-mode
+                    "<!--\\|<[^/>]>\\|<[^/][^>]*[^/]>"
+                    ""
+                    "<!--" ;; won't work on its own; uses syntax table
+                    (lambda (arg) (nxml-forward-element))
+                    nil))
+
+     (add-hook 'nxml-mode-hook
+               (lambda ()
+                 (make-local-variable 'outline-regexp)
+                 (setq outline-regexp "\\s *<\\([h][1-6]\\|html\\|body\\|head\\)\\b")))
+     ))
