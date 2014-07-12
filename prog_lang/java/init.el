faaae37fac55699831bed2e4a14cbd4db11894f9
Evgeny Budilovsky <budevg@gmail.com>
2014-07-12 20:44:42 +0300

    remove eclim for java which I never used

--- prog_lang/java/init.el
+++ prog_lang/java/init.el
@@ -1,11 +1,8 @@
-(autoload 'eclim-mode "eclim" nil t)
 (eval-after-load "cc-mode"
-  '(progn 
+  '(progn
      (defun my-java-mode-hook ()
        (c-set-style "java")
-       (setq c-basic-offset 2)
-       (if (executable-find "eclim")
-           (eclim-mode 1)))
+       (setq c-basic-offset 2))
 
      (add-hook 'java-mode-hook 'my-java-mode-hook)
 
