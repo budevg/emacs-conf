2fcd1bb257007b23fb811a7d831fdc5d972972ea
Evgeny Budilovsky <budevg@gmail.com>
2015-04-09 16:35:40 +0300

    go: add shortcuts for godef

--- prog_lang/go/init.el
+++ prog_lang/go/init.el
@@ -3,4 +3,7 @@
 (add-to-list 'auto-mode-alist '("\\.go$" . go-mode))
 (eval-after-load "go-mode"
   '(progn
-     (define-key go-mode-map "\C-m" 'newline-and-indent)))
+     (define-key go-mode-map "\C-m" 'newline-and-indent)
+     (define-key go-mode-map [(control /)] 'godef-jump)
+     (define-key go-mode-map [(control \?)] 'pop-tag-mark)
+     ))
