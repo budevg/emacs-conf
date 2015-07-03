ea81cbe093763dbfc99a3edb074eb6a3eec1a908
Evgeny Budilovsky <budevg@gmail.com>
2015-07-03 14:02:42 +0300

    yellow ebrowse fonts

--- look_and_feel/fonts.el
+++ look_and_feel/fonts.el
@@ -3,3 +3,4 @@
 (setq custom-safe-themes t)
 (add-to-list 'custom-theme-load-path (in-emacs-d "look_and_feel/themes"))
 (load-theme 'monokai t)
+(custom-set-faces '(ebrowse-root-class ((t (:foreground "yellow" :weight bold)))))
