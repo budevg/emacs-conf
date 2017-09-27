3bc1da441f7fb3f5efd540e475d2da2cd0db3066
Evgeny Budilovsky <budevg@gmail.com>
2017-09-27 22:09:42 +0300

    rust racer

--- prog_lang/rust/init.el
+++ prog_lang/rust/init.el
@@ -1,3 +1,9 @@
 (autoload 'rust-mode "rust-mode" nil t)
+(autoload 'racer-find-definition "racer" nil t)
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
 (add-to-list 'auto-mode-alist '("\\.toml\\'" . conf-mode))
+(eval-after-load "rust-mode"
+  '(progn
+     (define-key rust-mode-map [(control /)] 'racer-find-definition)
+     (define-key rust-mode-map [(control \?)] 'pop-tag-mark)
+     ))
