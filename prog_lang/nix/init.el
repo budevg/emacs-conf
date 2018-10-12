7178e0f0f02c0606f2f75d84ad8237fb38b250d8
Evgeny Budilovsky <budevg@gmail.com>
2018-10-12 20:16:57 +0300

    update nix mode

--- prog_lang/nix/init.el
+++ prog_lang/nix/init.el
@@ -1,3 +1,9 @@
 (autoload 'nix-mode "nix-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-mode))
 (add-to-list 'auto-mode-alist '("\\.nix.in\\'" . nix-mode))
+
+(eval-after-load "nix-mode"
+  '(progn
+     (setq nix-indent-function 'nix-indent-line)
+     (define-key nix-mode-map [(control P)] 'nix-repl)
+     ))
