dfa9e05c368aa934c497f9527f8c1eba2edba527
Evgeny Budilovsky <budevg@gmail.com>
2019-06-13 18:42:31 +0300

    elm format and snippets

--- prog_lang/elm/init.el
+++ prog_lang/elm/init.el
@@ -1,2 +1,7 @@
 (autoload 'elm-mode "elm-mode" nil t)
+(autoload 'elm-format-buffer "elm-format" nil t)
 (add-to-list 'auto-mode-alist '("\\.elm\\'" . elm-mode))
+(eval-after-load "elm-mode"
+  '(progn
+     (define-key elm-mode-map (kbd "C-c C-p") 'elm-format-buffer)
+     ))
