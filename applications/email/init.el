0389541cf4c17b8bf7bfdb7373ad2513d3ee8252
Evgeny Budilovsky <budevg@gmail.com>
2019-09-28 18:35:36 +0300

    mu4e consolidate account management

--- applications/email/init.el
+++ applications/email/init.el
@@ -16,15 +16,38 @@
       (autoload 'mu4e "mu4e" nil t)
       (eval-after-load "mu4e"
         '(progn
-           (defun mu4e-account1 (prefix)
+           (defun mu4e-account-context (prefix)
+             (interactive)
+             (make-mu4e-context
+              :name prefix
+              :enter-func `(lambda () (mu4e-account ,prefix))
+              ))
+           (defun mu4e-account (prefix)
              (interactive)
              (mu4e-message (format "mu4e-account %s" prefix))
-             (setq
-              mu4e-inbox-folder (format "/%s/Inbox" prefix)
-              mu4e-drafts-folder (format "/%s/[Gmail]/Drafts" prefix)
-              mu4e-sent-folder (format "/%s/[Gmail]/Sent Mail" prefix)
-              mu4e-trash-folder (format "/%s/[Gmail]/Trash" prefix)
+             (cond
+              ((string= prefix "AAA")
+               (setq
+                mu4e-inbox-folder (format "/%s/Inbox" prefix)
+                mu4e-drafts-folder (format "/%s/[Gmail]/Drafts" prefix)
+                mu4e-sent-folder (format "/%s/[Gmail]/Sent Mail" prefix)
+                mu4e-trash-folder (format "/%s/[Gmail]/Trash" prefix)
+                smtpmail-smtp-server "smtp.gmail.com"
+                user-mail-address (format "%s@gmail.com" prefix)
+                user-full-name  prefix)
+               )
+              ((string= prefix "BBB")
+               (setq
+                mu4e-inbox-folder (format "/%s/Inbox" prefix)
+                mu4e-drafts-folder (format "/%s/Drafts" prefix)
+                mu4e-sent-folder (format "/%s/Sent" prefix)
+                mu4e-trash-folder (format "/%s/Deleted" prefix)
+                smtpmail-smtp-server "smtp.office365.com"
+                user-mail-address (format "%s@outlook.com" prefix)
+                user-full-name prefix
+                )))
 
+             (setq
               mu4e-maildir-shortcuts `((,mu4e-inbox-folder . ?i)
                                        (,mu4e-sent-folder  . ?s)
                                        (,mu4e-trash-folder . ?t)
@@ -43,14 +66,11 @@
                                   :query "flag:flagged"
                                   :key ?f))
 
-              user-mail-address "AAA@gmail.com"
-              user-full-name  "AAA"
               mu4e-compose-signature (format "Best Regards,\n%s\n" user-full-name)
 
 
               mu4e-get-mail-command (format "mbsync -c %s %s" (in-emacs-d "applications/email/.mbsyncrc") prefix)
 
-              smtpmail-smtp-server "smtp.gmail.com"
               smtpmail-default-smtp-server smtpmail-smtp-server
               smtpmail-local-domain smtpmail-smtp-server
               smtpmail-smtp-user user-mail-address
@@ -65,12 +85,8 @@
 
                  mu4e~get-mail-password-regexp "^Password.*: $"
                  mu4e-contexts
-                 `( ,(make-mu4e-context
-                      :name "AAA"
-                      :enter-func (lambda () (mu4e-account1 "AAA"))
-
-                      )
-
+                 `( ,(mu4e-account-context "AAA")
+                    ,(mu4e-account-context "BBB")
                     )
 
                  mu4e-context-policy 'pick-first
