d25cecbab3018334bd9519a1fbf519083ff53d06
Evgeny Budilovsky <budevg@gmail.com>
2019-12-11 23:30:03 +0200

    prog_lang: format-all to format all languages

--- prog_lang/smart_code/syntax_checking/init.el
+++ prog_lang/smart_code/syntax_checking/init.el
@@ -1,3 +1,6 @@
 (autoload 'flycheck-mode "flycheck" nil t)
 (global-set-key [(control O)] 'flycheck-mode)
 (setq flycheck-python-pylint-executable "pylint")
+
+(autoload 'format-all-buffer "format-all" nil t)
+(global-set-key (kbd "C-c p") 'format-all-buffer)
