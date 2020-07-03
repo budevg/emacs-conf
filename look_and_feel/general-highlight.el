0eebb935cd68fdbbf796a00cdcb6bf46618208ec
Evgeny Budilovsky <budevg@gmail.com>
2020-07-03 11:54:39 +0300

    fix hi-lock-set-pattern

--- look_and_feel/general-highlight.el
+++ look_and_feel/general-highlight.el
@@ -79,7 +79,7 @@
 (autoload 'rainbow-mode "rainbow-mode" nil t)
 
 ;; allow persistent highlighting in every mode
-(defadvice hi-lock-set-pattern (around hi-lock-add-keyword-advice (regexp face))
+(defadvice hi-lock-set-pattern (around hi-lock-add-keyword-advice (regexp face &optional subexp))
   (let ((font-lock-fontified t))
     ad-do-it))
 (ad-activate 'hi-lock-set-pattern)
