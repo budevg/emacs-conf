ad2e0db9b4592e5d819c4952187ed068eb342f8b
Evgeny Budilovsky <budevg@gmail.com>
2020-12-26 21:04:46 +0200

    remove deprecated marmalade package archive

--- packages/init.el
+++ packages/init.el
@@ -1,7 +1,6 @@
 (autoload 'package-list-packages "package" nil t)
 (autoload 'package-list-packages-no-fetch "package" nil t)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))
 
 (setq el-get-recipe-path (list (in-emacs-d "packages/el-get/recipes-private")))
