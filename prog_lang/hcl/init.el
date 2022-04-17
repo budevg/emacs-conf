e37e40330fba11daa4f8487365a64601bcfc19ac
Evgeny Budilovsky <budevg@gmail.com>
2022-04-17 09:03:02 +0300

    syntax highlighting for hcl files

--- prog_lang/hcl/init.el
+++ prog_lang/hcl/init.el
@@ -1,2 +1,5 @@
 (autoload 'terraform-mode "terraform-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.tf\\(vars\\)?\\'" . terraform-mode))
+
+(autoload 'hcl-mode "hcl-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.hcl$" . hcl-mode))
