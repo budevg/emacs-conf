bcd77536ef71ef6fb7be36d689644b397136d46f
Evgeny Budilovsky <budevg@gmail.com>
2023-04-12 15:53:47 +0300

    compat package

--- autogen/configuration.py
+++ autogen/configuration.py
@@ -9,6 +9,8 @@ class Node(object):
 
 lisp_lib = [Node(path = "lisp_lib/init.el",
                  msg  = "Misc elisp function"),
+            Node(path = "lisp_lib/compat",
+                 msg  = ""),
             Node(path = "lisp_lib/idle-require.el",
                  msg  = "Load packages when emacs idle"),
             ]
