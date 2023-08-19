c978ec9a8aec7af368d3f14c4a8af85a01ac8388
Evgeny Budilovsky <budevg@gmail.com>
2023-08-19 20:05:18 +0300

    refactor cmake mode

--- build_system/init.el
+++ build_system/init.el
@@ -1,6 +1,3 @@
-
-(autoload 'cmake-mode "cmake-mode" nil t)
-(setq auto-mode-alist
-      (append '(("CMakeLists\\.txt\\'" . cmake-mode)
-                ("\\.cmake\\'" . cmake-mode))
-              auto-mode-alist))
+(use-package cmake-mode
+  :mode ("CMakeLists\\.txt\\'" "\\.cmake\\'")
+  )
