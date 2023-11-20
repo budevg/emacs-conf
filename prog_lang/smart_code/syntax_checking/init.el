fabb365aba5ce14e191029ccb88a07d27207dc31
Evgeny Budilovsky <budevg@gmail.com>
2023-11-20 08:11:23 +0200

    support formatting of region

--- prog_lang/smart_code/syntax_checking/init.el
+++ prog_lang/smart_code/syntax_checking/init.el
@@ -2,8 +2,8 @@
 (global-set-key [(control O)] 'flycheck-mode)
 (setq flycheck-python-pylint-executable "pylint")
 
-(autoload 'format-all-buffer "format-all" nil t)
-(global-set-key (kbd "C-c p") 'format-all-buffer)
+(autoload 'format-all-region-or-buffer "format-all" nil t)
+(global-set-key (kbd "C-c p") 'format-all-region-or-buffer)
 (setq format-all-formatters
       '(("Python" black)
         ("Go" goimports)
