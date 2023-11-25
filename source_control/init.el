5091aefb535d9a1e95392dc9a1011cd489a9d885
Evgeny Budilovsky <budevg@gmail.com>
2023-11-25 15:41:55 +0200

    ediff settings

--- source_control/init.el
+++ source_control/init.el
@@ -25,3 +25,11 @@
      (add-hook 'diff-mode-hook
                (lambda ()
                  (setq diff-auto-refine-mode nil)))))
+
+(setq ediff-keep-variants nil
+      ediff-make-buffers-readonly-at-startup nil
+      ediff-merge-revisions-with-ancestor t
+      ediff-show-clashes-only t
+      ediff-split-window-function 'split-window-horizontally
+      ediff-window-setup-function 'ediff-setup-windows-plain
+      ediff-highlight-all-diffs nil)
