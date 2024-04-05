686090266820ab4aab26d707c70a8052d7df9595
Evgeny Budilovsky <budevg@gmail.com>
2024-04-05 16:58:16 +0300

    configure elfeed-summary

--- applications/elfeed/init.el
+++ applications/elfeed/init.el
@@ -1,42 +1,75 @@
-(autoload 'elfeed "elfeed" nil t)
-(eval-after-load "elfeed"
-  `(progn
-     (require 'elfeed-org)
-     (elfeed-org)
+(use-package elfeed-summary
+  :commands (elfeed-summary)
+  :config
+  (setq elfeed-summary-default-filter ""
+        elfeed-summary--only-unread t
+        elfeed-summary-settings
+        '((auto-tags (:original-order . t)
+                     )
+          )))
 
-     (setq elfeed-search-remain-on-entry t)
 
-     (defun elfeed-show-url-in-eww (url &optional arg)
-       (with-current-buffer (generate-new-buffer "*feed*")
-         (eww-mode)
-         (eww url)))
+(use-package elfeed
+  :after (elfeed-summary)
 
-     (defun elfeed-show-eww-open ()
-       (interactive)
-       (let ((browse-url-browser-function #'elfeed-show-url-in-eww))
-         (elfeed-show-visit nil)))
+  :custom
+  (elfeed-search-title-max-width 120)
 
-     (defun elfeed-search-eww-open ()
-       (interactive)
-       (let ((browse-url-browser-function #'elfeed-show-url-in-eww))
-         (elfeed-search-browse-url nil)))
+  :config
+  (defun elfeed-show-url-in-eww (url &optional arg)
+    (with-current-buffer (generate-new-buffer "*feed*")
+      (eww-mode)
+      (eww url)))
 
-     (define-key elfeed-show-mode-map (kbd "/") 'elfeed-show-eww-open)
-     (define-key elfeed-search-mode-map (kbd "/") 'elfeed-search-eww-open)
-     (define-key elfeed-show-mode-map (kbd "C-/") 'elfeed-show-visit)
-     (define-key elfeed-search-mode-map (kbd "C-/") 'elfeed-search-browse-url)
+  (defun elfeed-show-eww-open ()
+    (interactive)
+    (let ((browse-url-browser-function #'elfeed-show-url-in-eww))
+      (elfeed-show-visit nil)))
 
-     (mapcar
-      (lambda (e)
-        (let ((k (car e))
-              (f (cdr e)))
-          (define-key elfeed-search-mode-map (kbd k)
-            `(lambda () (interactive) (elfeed-search-set-filter ,f)))
-          ))
-      '(("1" . "@3-days-ago +unread")
-        ("2" . "@1-weeks-ago +unread")
-        ("3" . "@1-mongths-ago +unread")
-        ("4" . "+unread"))
-      )
+  (defun elfeed-search-eww-open ()
+    (interactive)
+    (let ((browse-url-browser-function #'elfeed-show-url-in-eww))
+      (elfeed-search-browse-url nil)))
+  7
+  (define-key elfeed-show-mode-map (kbd "/") 'elfeed-show-eww-open)
+  (define-key elfeed-search-mode-map (kbd "/") 'elfeed-search-eww-open)
+  (define-key elfeed-show-mode-map (kbd "C-/") 'elfeed-show-visit)
+  (define-key elfeed-search-mode-map (kbd "C-/") 'elfeed-search-browse-url)
 
-     ))
+  (mapcar
+   (lambda (e)
+     (let ((k (car e))
+           (f (cdr e)))
+       (define-key elfeed-search-mode-map (kbd k)
+                   `(lambda () (interactive) (elfeed-search-set-filter ,f)))
+       ))
+   '(("1" . "@3-days-ago +unread")
+     ("2" . "@1-weeks-ago +unread")
+     ("3" . "@1-mongths-ago +unread")
+     ("4" . "+unread"))
+   )
+
+  (defun elfeed-custom-sort (a b)
+    (let* ((a-tags (format "%s" (elfeed-entry-tags a)))
+           (b-tags (format "%s" (elfeed-entry-tags b)))
+           (a-title (elfeed-feed-title (elfeed-entry-feed a)))
+           (b-title (elfeed-feed-title (elfeed-entry-feed b))))
+      (if (string= a-tags b-tags)
+          (if (string= a-title b-title)
+              (< (elfeed-entry-date b) (elfeed-entry-date a))
+            (string< b-title a-title))
+        (string< a-tags b-tags))))
+
+  (defun elfeed-entry-reformat (buff)
+    (switch-to-buffer buff)
+    (writing-mode 1)
+    (elfeed-show-refresh))
+
+  (setf elfeed-search-sort-function #'elfeed-custom-sort)
+  (setq elfeed-show-entry-switch 'elfeed-entry-reformat
+        elfeed-search-remain-on-entry t))
+
+(use-package elfeed-org
+  :after (elfeed)
+  :config
+  (elfeed-org))
