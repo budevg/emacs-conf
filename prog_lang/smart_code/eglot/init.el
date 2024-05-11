7f437d60f83fd9245af6d882d372634d2574f4ca
Evgeny Budilovsky <budevg@gmail.com>
2024-05-11 13:43:44 +0300

    configure eglot with use-package

--- prog_lang/smart_code/eglot/init.el
+++ prog_lang/smart_code/eglot/init.el
@@ -1,27 +1,27 @@
-(autoload 'eglot "eglot" nil t)
+(use-package eglot
+  :commands (eglot)
+  :config
+  (setq eglot-autoshutdown t
+        eglot-stay-out-of '(eldoc flymake company imenu)
+        eglot-extend-to-xref t
+        eglot-events-buffer-size 0
+        eglot-ignored-server-capabilities '(:completionProvider :inlayHintProvider)
+        )
+  (add-to-list 'eglot-server-programs
+               '(java-mode . ("java-language-server")))
+  (add-to-list 'eglot-server-programs
+               `((js-mode typescript-mode jsx-mode) .
+                 ,(eglot-alternatives
+                   '(("deno" "lsp") ("typescript-language-server" "--stdio")))))
+  (defun eglot--post-self-insert-hook ())
+  :bind
+  (:map eglot-mode-map
+        ("C-/" .  'xref-find-definitions)
+        ("C-?" . 'xref-go-back)
+        ("C-." .  'xref-find-references)
+        ("C-c P" .  'eglot-format))
+  )
 
 (if (not (fboundp 'project-root))
     (cl-defgeneric project-root (project)
       (car (project-roots project))))
-
-(eval-after-load "eglot"
-  '(progn
-     (setq eglot-autoshutdown t
-           eglot-stay-out-of '(eldoc flymake company imenu)
-           eglot-extend-to-xref t
-           eglot-ignored-server-capabilities '(:completionProvider :inlayHintProvider)
-           )
-
-     (add-to-list 'eglot-server-programs
-                  '(java-mode . ("java-language-server")))
-     (add-to-list 'eglot-server-programs
-                  `((js-mode typescript-mode jsx-mode) .
-                    ,(eglot-alternatives
-                      '(("deno" "lsp") ("typescript-language-server" "--stdio")))))
-
-     (define-key eglot-mode-map (kbd "C-/")  'xref-find-definitions)
-     (define-key eglot-mode-map (kbd "C-?") 'xref-go-back)
-     (define-key eglot-mode-map (kbd "C-.")  'xref-find-references)
-     (define-key eglot-mode-map (kbd "C-c P")  'eglot-format)
-     (defun eglot--post-self-insert-hook ())
-     ))
