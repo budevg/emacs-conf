67a469958531f7aa1be8f1fe84d20a90766db27c
Evgeny Budilovsky <budevg@gmail.com>
2024-11-22 18:27:50 +0200

    add bazel mode

--- build_system/init.el
+++ build_system/init.el
@@ -1,3 +1,10 @@
 (use-package cmake-mode
   :mode ("CMakeLists\\.txt\\'" "\\.cmake\\'")
   )
+
+(use-package bazel
+  :mode (("\\.bazel\\'" . bazel-mode)
+         ("\\.bzl\\'" . bazel-mode)
+         ("\\BUILD\\'" . bazel-mode)
+         ("\\WORKSPACE\\'" . bazel-mode))
+  )
