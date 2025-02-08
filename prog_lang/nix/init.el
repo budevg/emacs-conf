97e2f3ba7982cbc8aeceba2d64a2406c821f5e62
Evgeny Budilovsky <budevg@gmail.com>
2025-02-08 11:49:20 +0200

    don't pass nix-shell TMPDIR environment variables
    In
    latest (nix)[https://github.com/NixOS/nix/commit/2105574702b582578c43b551cfe8905715211f03]
    these variables set to $TMPDIR/nix-shell-xxx sub directory which
    is deleted on nix=shell exit this will cause problems to various tools which
    will try to access temp directory and fail.

--- prog_lang/nix/init.el
+++ prog_lang/nix/init.el
@@ -1,13 +1,10 @@
-(autoload 'nix-mode "nix-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-mode))
-(add-to-list 'auto-mode-alist '("\\.nix.in\\'" . nix-mode))
-
-(eval-after-load "nix-mode"
-  '(progn
-     (setq nix-indent-function 'nix-indent-line)
-     (define-key nix-mode-map (kbd "C-S-p") 'nix-repl)
-     (define-key nix-mode-map (kbd "C-c p") 'nix-format-buffer)
-     ))
-
-(autoload 'nix-env-load-or-reset "nix-env" nil t)
-(global-set-key (kbd "C-f x") 'nix-env-load-or-reset)
+(use-package nix-mode
+  :mode ("\\.nix\\'" "\\.nix.in\\'")
+  :init
+  (autoload 'nix-env-load-or-reset "nix-env" nil t)
+  :config
+  (setq nix-indent-function 'nix-indent-line)
+  (define-key nix-mode-map (kbd "C-S-p") 'nix-repl)
+  (define-key nix-mode-map (kbd "C-c p") 'nix-format-buffer)
+  :bind ("C-f x" . nix-env-load-or-reset)
+  )
