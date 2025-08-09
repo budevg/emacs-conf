027d1aca9a7bc69ed2efe9a770dc524188871fdb
Evgeny Budilovsky <budevg@gmail.com>
2025-08-09 16:17:06 +0300

    refactor shell and comint mode

--- shell/shell-buffer.el
+++ shell/shell-buffer.el
@@ -2,67 +2,63 @@
 (setq kill-buffer-query-functions
       (remq 'process-kill-buffer-query-function kill-buffer-query-functions))
 
-(add-to-list 'display-buffer-alist
-             '("^\\*shell\\*" . (display-buffer-same-window)))
+(use-package shell
+  :defer t
+  :config
+  (add-to-list 'display-buffer-alist
+               '("^\\*shell\\*" . (display-buffer-same-window)))
 
-(defun new-shell ()
-  "create new shell"
-  (interactive)
-  (shell (generate-new-buffer "*shell*"))
-)
+  (defun new-shell ()
+    "create new shell"
+    (interactive)
+    (shell (generate-new-buffer "*shell*"))
+    )
 
-(defun shell-command-on-region-inplace ()
-  (interactive)
-  (let ((command (read-shell-command "Shell command on region: ")))
-    (shell-command-on-region (region-beginning) (region-end) command t t)))
+  (defun shell-command-on-region-inplace ()
+    (interactive)
+    (let ((command (read-shell-command "Shell command on region: ")))
+      (shell-command-on-region (region-beginning) (region-end) command t t)))
 
-(defun shell-change-to-current-dir ()
-  (interactive)
-  (let ((current-directory-path (expand-file-name default-directory)))
-    (shell)
-    (end-of-buffer)
-    (insert "cd \"" )
-    (insert current-directory-path)
-    (insert "\"" )
-    (comint-send-input)))
+  :bind (("<f2>" . shell)
+         ("C-<f2>" . new-shell)
+         ("M-<f2>" . shell-command-on-region-inplace)
+         )
+  )
 
-(global-set-key [(f2)] 'shell)
-(global-set-key [(control f2)] 'new-shell)
-(global-set-key [(meta f2)] 'shell-command-on-region-inplace)
-
-(eval-after-load "comint"
-  '(progn
-     ;;["black" "red" "green" "yellow" "blue" "magenta" "cyan" "white"]
-     ;; change blue->yellow
-     (defun comint-previous-matching-input-from-input (n)
-       "Search backwards through input history for match for current input.
+(use-package comint
+  :defer t
+  :config
+  (defun comint-previous-matching-input-from-input (n)
+    "Search backwards through input history for match for current input.
 \(Previous history elements are earlier commands.)
 With prefix argument N, search for Nth previous match.
 If N is negative, search forwards for the -Nth following match."
-       (interactive "p")
-       (let ((opoint (point)))
-         (unless (memq last-command '(comint-previous-matching-input-from-input
-                                      comint-next-matching-input-from-input))
-           ;; Starting a new search
-           (setq comint-matching-input-from-input-string
-                 (buffer-substring
-                  (or (marker-position comint-accum-marker)
-                      (process-mark (get-buffer-process (current-buffer))))
-                  (point))
-                 comint-input-ring-index nil))
-         (comint-previous-matching-input
-          (concat "^" (regexp-quote comint-matching-input-from-input-string))
-          n)
-         ;;(goto-char opoint)
-         ))
+    (interactive "p")
+    (let ((opoint (point)))
+      (unless (memq last-command '(comint-previous-matching-input-from-input
+                                   comint-next-matching-input-from-input))
+        ;; Starting a new search
+        (setq comint-matching-input-from-input-string
+              (buffer-substring
+               (or (marker-position comint-accum-marker)
+                   (process-mark (get-buffer-process (current-buffer))))
+               (point))
+              comint-input-ring-index nil))
+      (comint-previous-matching-input
+       (concat "^" (regexp-quote comint-matching-input-from-input-string))
+       n)
+      ;;(goto-char opoint)
+      ))
 
-     (ansi-color-for-comint-mode-on)
-     (define-key comint-mode-map [(meta p)] nil)
-     (define-key comint-mode-map [C-up] 'comint-previous-matching-input-from-input)
-     (define-key comint-mode-map [C-down] 'comint-next-matching-input-from-input)
-     (define-key comint-mode-map (kbd "C-c k") 'comint-clear-buffer))
+  (ansi-color-for-comint-mode-on)
+
+  :bind (:map comint-mode-map
+         ("M-p" . nil)
+         ("C-<up>" . comint-previous-matching-input-from-input)
+         ("C-<down>" . comint-next-matching-input-from-input)
+         ("C-c C-k" . comint-clear-buffer)
+         )
   )
-
 
 (use-package vterm
   :commands (vterm)
